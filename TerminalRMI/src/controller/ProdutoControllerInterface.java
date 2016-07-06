753f8d11b4ca4038e530b547e492dc55f3f51d6e
AngelitaNunes <angelita.nunes@outlook.com>
2016-07-05 22:47:56 -0300

    alterações na interface

--- TerminalRMI/src/controller/ProdutoControllerInterface.java
+++ TerminalRMI/src/controller/ProdutoControllerInterface.java
@@ -16,12 +16,14 @@ import java.util.List;
  *
  * @author viniciuscustodio
  */
-public interface ProdutoControllerInterface extends Remote {
-
+public interface ProdutoControllerInterface extends Remote{
     public List<Produto> getProducts() throws RemoteException;
     public void imprimirMensagem(String nickname, String mensagem) throws RemoteException;
-
     public boolean realizarPedido(List<Pedido> pedido) throws RemoteException;
     public Produto getProduct(Long idProduto) throws RemoteException;
+    //Esse método vai receber o produto e se esse produto será cadastrado -1 ou editado -0
+    //se for para cadastrar ele chama o método de inserção de novo item, se for edição, chama o de atualização
+    public boolean cadastrarProduto(Produto produto, boolean cadastrar) throws RemoteException;
+    public boolean excluirProduto(Produto produto) throws RemoteException;
 
 }
